12adc9af34df5b46471891cd8b69d35d45368a6c
Gabriel Oliveira <gabriel.golvs@gmail.com>
2019-06-17 17:56:31 -0300

    metodo da divisao

--- man_data.c
+++ man_data.c
@@ -16,41 +16,48 @@ typedef struct _pessoa //144 bytes
 } Pessoa;
 
 Pessoa parseData(char linha[]);
+int metodo_divisao(int x);
 
 int main(){
-    int i = 0;
+    int i = 0, pos;
     FILE *p = NULL;
     char linha[200];
     Pessoa hash[N];
-
+    Pessoa temp;
     //abrindo o arquivo
     p =  fopen("dados/dados-500000-lin.csv", "r");
     
     //verificando se foi aberto
     if(p == NULL){
-        return ;
+        return 0;
     }
     //essa linha vai ler o cabeçalho, para que a iteração desconsidere ele como um dado valido
     fgets(linha, sizeof(linha), p);
 
     //lendo os dados
-    while(i<N){
+    while(i<1){
         fgets(linha, sizeof(linha), p);
-        hash[i] = parseData(linha);
+        printf("%s\n", linha);
+        
+        temp = parseData(linha);
+        printf("Nome: %s\n", temp.nome);
+        printf("id: %d\n", temp.linha);
+        //pos = metodo_divisao(temp.chave);
+        printf("posição no array: %d\n", pos);
         i++;
     }
 
-/* 
-    for(int j = 0; j<i; j++){
-        printf("pos: %d - %s\n",j,  hash[j].nome);
-    }
-*/
     fclose(p);
+    return 0;
  
 }
 
-Pessoa parseData(char linha[]){
+int metodo_divisao(int x){
+    return x%N;
+}
 
+Pessoa parseData(char linha[]){
+    //ID NÃO ESTÁ FUNCIONANDO
     Pessoa r;
     char *pt;
     pt = strtok (linha, ",");
@@ -67,5 +74,6 @@ Pessoa parseData(char linha[]){
     strncpy(r.email, pt, 20); // email
     pt = strtok (NULL, ",");
     strncpy(r.celular, pt, 13); // celular
+    printf("FUNCAO:\nid: %d\n", r.chave);
     return r;
 }
